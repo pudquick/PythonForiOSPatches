23b740be80fb59fd473811c00b932bb569a33ea7
Michael Lynn <pudquick@sogetthis.com>
2012-01-31 11:42:57 -0800

    added _rp.py

---
+++ _rp.py
@@ -0,0 +1,15 @@
+import os as os
+import sys as sys
+
+class iPy:
+    dir = [x for x in sys.path if ('iPython.app' in x)][0].split('iPython.app')[0]
+    docs = dir + 'Documents'
+    mods = docs + '/modules'
+    scripts = docs + '/User Scripts'
+
+ipython = iPy()
+
+if not (ipython.mods in sys.path):
+    sys.path.append(ipython.mods + '')
+os.chdir(ipython.scripts)
+
